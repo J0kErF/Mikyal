a5275f2684a9b6cbac7d4144d59976538c7ed564
Mohammad Yosef <mohmd.sy2002@gmail.com>
2023-11-27 19:03:39 +0200

    first push of mvp

---
+++ src/demo/google_bard.py
@@ -0,0 +1,14 @@
+import google.generativeai as paml
+from django.conf import settings
+import os
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(settings.BASE_DIR, "gen-lang-client-0038497942-3a8cdaf7064f.json")
+
+paml.configure(api_key=settings.PAML_APP_KEY)
+
+def get_response(prompt):
+    prompt="based on these 10 question tell if the content of the link is biased ....Are stories on important issues featured prominently? Do the headlines and stories match? Is there a lack of context? Is the language loaded? What are the unchallenged assumptions? Do stereotypes skew coverage? Are there double standards? From whose point of view is the news reported? Is there a lack of diversity? Who are the sources? and the url is "+prompt
+    response= paml.chat(messages=prompt)
+    print(response)
+    return response.last
